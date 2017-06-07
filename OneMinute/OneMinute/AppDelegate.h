77ef3468e762b77dd18ea39393bf82298770ed9a
邓柯 <simplelife.patrick@gmail.com>
2017-06-07 15:18:05 +0800

    Xcode工程初始化

---
+++ OneMinute/OneMinute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneMinute
+//
+//  Created by 邓柯 on 2017/6/7.
+//  Copyright © 2017年 动旅游. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneMinute/OneMinute/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OneMinute
+//
+//  Created by 邓柯 on 2017/6/7.
+//  Copyright © 2017年 动旅游. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
