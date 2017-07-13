c619a710de47bc02aab20b7b90cb912a5513f031
陈立勇 <1138880439@@qq.com>
2017-07-13 14:39:51 +0800

    添加导航控制器

---
+++ OneMinute/OneMinute/DLYBaseNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  DLYBaseNavigationController.m
+//  OneMinute
+//
+//  Created by 陈立勇 on 2017/7/13.
+//  Copyright © 2017年 动旅游. All rights reserved.
+//
+
+#import "DLYBaseNavigationController.h"
+
+@interface DLYBaseNavigationController ()
+
+@end
+
+@implementation DLYBaseNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
