e139eff326fa37fd1d751b40a7ed02de12d838a1
陈宗海 <cherklouis2@gmail.com>
2017-07-14 13:49:12 +0800

    提交修改

---
+++ OneMinute/OneMinute/AVEngine/DLYMediaEditManage.h
@@ -0,0 +1,15 @@
+//
+//  DLYMediaEditManage.h
+//  OneMinute
+//
+//  Created by chenzonghai on 12/07/2017.
+//  Copyright © 2017 动旅游. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DLYMediaEditManage : NSObject
+
+
+
+@end
