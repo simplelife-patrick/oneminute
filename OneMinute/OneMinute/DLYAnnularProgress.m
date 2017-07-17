f1024a66f3359452b3910a7c165077eca71b268c
陈立勇 <1138880439@qq.com>
2017-07-17 15:16:37 +0800

    播放视频，启动播放，左右手，界面

---
+++ OneMinute/OneMinute/DLYAnnularProgress.m
@@ -0,0 +1,88 @@
+//
+//  DLYAnnularProgress.m
+//  OneMinute
+//
+//  Created by 陈立勇 on 2017/7/14.
+//  Copyright © 2017年 动旅游. All rights reserved.
+//
+
+#import "DLYAnnularProgress.h"
+
+@implementation DLYAnnularProgress
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
+- (id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        
+    }
+    
+    return self;
+}
+
+
+- (void)drawCycleProgress
+{
+    CGPoint center = self.center;
+    CGFloat radius = _circleRadius;
+    CGFloat startA = -M_PI_2 + M_PI * 2 * _progress;  //设置进度条起点位置
+    CGFloat endA = -M_PI_2;  //设置进度条终点位置
+    
+    //获取环形路径（画一个圆形，填充色透明，设置线框宽度为10，这样就获得了一个环形）
+    _progressLayer = [CAShapeLayer layer];//创建一个track shape layer
+    _progressLayer.frame = self.bounds;
+    _progressLayer.fillColor = _fillColor ? _fillColor.CGColor : [UIColor clearColor].CGColor;//填充色为无色
+    _progressLayer.strokeColor = _lineColor ? _lineColor.CGColor : RGB(255, 0, 0).CGColor;//指定path的渲染颜色,这里可以设置任意不透明颜色
+    _progressLayer.opacity = 1; //背景颜色的透明度
+    _progressLayer.lineCap = kCALineCapRound;//指定线的边缘是圆的
+    _progressLayer.lineWidth =  _lineWidth ? _lineWidth : 2.0;//线的宽度
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:startA endAngle:endA clockwise:YES];//上面说明过了用来构建圆形
+    _progressLayer.path =[path CGPath]; //把path传递給layer，然后layer会处理相应的渲染，整个逻辑和CoreGraph是一致的。
+    [self.layer addSublayer:_progressLayer];
+    
+    
+}
+
+- (void)drawProgress:(CGFloat )progress
+{
+    [UIView animateWithDuration:1.0f animations:^{
+        _progress =progress;
+        
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    
+    _progressLayer.opacity = 0;
+    [self setNeedsDisplay];
+}
+
+- (void)drawProgress:(CGFloat )progress withColor:(UIColor *)color
+{
+    [UIView animateWithDuration:1.0f animations:^{
+        _progress =progress;
+        
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    _progressLayer.strokeColor = color.CGColor;
+    [self setNeedsDisplay];
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    
+    [self drawCycleProgress];
+    
+}
+
+
+
+@end
---
+++ OneMinute/OneMinute/UIImage+iconfont.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+iconfont.h
+//  test
+//
+//  Created by 两幅画 on 2017/6/26.
+//  Copyright © 2017年 两幅画. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (iconfont)
+
++ (UIImage*)imageWithIcon:(NSString*)iconCode inFont:(NSString*)fontName size:(NSUInteger)size color:(UIColor*)color;
+@end
