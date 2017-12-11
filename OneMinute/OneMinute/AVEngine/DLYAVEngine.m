5b12d71107a6dcacc02cb48754439248de7c9480
刘大靖 <iamldj@163.com>
2017-12-11 11:09:15 +0800

    解决点击屏幕重设曝光点失效问题

--- OneMinute/OneMinute/AVEngine/DLYAVEngine.m
+++ OneMinute/OneMinute/AVEngine/DLYAVEngine.m
@@ -595,6 +595,11 @@
 #pragma mark - 点触设置曝光 -
 
 -(void)focusContinuousWithPoint:(CGPoint)point{
+    //AVCaptureVideoPreviewLayer传的点是(0,0)到(1,1)之间,GLKView获得的是屏幕像素点,需要转换
+    if (point.x>1) {
+        point.x = point.x/SCREEN_WIDTH*1.0;
+        point.y = point.y/SCREEN_HEIGHT*1.0;
+    }
     
     AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
